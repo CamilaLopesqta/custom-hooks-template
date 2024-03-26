4045a1e36c0794a35a284e4e623adf21fa849aa9
Camila Lopes <camilalopesqta@gmail.com>
2024-03-26 15:51:11 -0300

    Exercícios de Custom Hooks

--- src/pages/CommentsPage.js
+++ src/pages/CommentsPage.js
@@ -1,34 +1,38 @@
-import {Title,PostContainer } from '../style'
-import { Card } from '../components/Card/Card'
-import { useCapturarPostagem } from '../hooks/useCapturarPostagem'
-import { useRequestData } from '../hooks/useRequestData'
-import { BASE_URL } from '../constants/constants'
+import { Title, PostContainer } from "../style";
+import { Card } from "../components/Card/Card";
+import { useCapturarPostagem } from "../hooks/useCapturarPostagem";
+import { useRequestData } from "../hooks/useRequestData";
+import { BASE_URL } from "../constants/constants";
 
-const  CommentsPage = () => {
-//const [posts] = useCapturarPostagem()
-  const [posts] = useRequestData([], `${BASE_URL}comments`)
+const CommentsPage = () => {
+  //const [posts] = useCapturarPostagem()
+  const [posts, error, isLoading] = useRequestData([], `${BASE_URL}comments`);
 
   return (
     <div>
       <Title>Comentários dos usuários</Title>
       <PostContainer>
-
-      {posts.map((post) => {
-        //console.log(post);
-        return(
-          <Card 
-          key={post.id} 
-          text={post.body} 
-          backgroudColor={'#1dc690'}
-          textColor={'#ffffff'}
-          />)
-      })}
+        {isLoading && <p>Carregando ...</p>}
+        {!isLoading && error && (
+          <p>Ocorreu um erro. Tente novamente mais tarde.</p>
+        )}
+        {!isLoading && !error && posts && posts.length === 0 && (
+          <p>Lista vazia.</p>
+        )}
+        {posts.map((post) => {
+          //console.log(post);
+          return (
+            <Card
+              key={post.id}
+              text={post.body}
+              backgroudColor={"#1dc690"}
+              textColor={"#ffffff"}
+            />
+          );
+        })}
       </PostContainer>
     </div>
   );
-}
+};
 
 export default CommentsPage;
-
-
-
